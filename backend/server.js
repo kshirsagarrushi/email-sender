81bd2a70e8ef773c4947495e3565331faa8d6828
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2024-04-15 13:37:25 +0530

    vercel json file added

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||80001;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
